d280f1a63d4d7b2d12cf2ab270cbbe3bdf8709d9
Alexey <iloveyoucreator@gmail.com>
2020-03-27 10:31:31 +0300

    ссылочки - хуилочки

---
+++ literature.js
@@ -0,0 +1,19 @@
+const literature = {
+	"Программа": [
+		"lol",
+		"kek",
+		"Lab2.docx"
+	],
+	"Теория": [
+		"cheburek",
+		"huek"
+	],
+	"Практика": [
+		"alalak",
+		"efwfwf"
+	],
+	"Контроль знаний": [
+		"zhopa",
+		"hui"
+	]
+}
