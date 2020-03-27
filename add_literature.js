318b8e3afd7ce929461fed790a221b7827907ca0
Alexey <iloveyoucreator@gmail.com>
2020-03-27 10:34:22 +0300

    fix

--- add_literature.js
+++ add_literature.js
@@ -1,7 +1,7 @@
 console.log(window.location.pathname.replace("/index.html",''));
 for(let book of literature["Программа"]) {
 	var li = document.createElement('li');
-	li.innerHTML = `<a href="https://docs.google.com/gview?url='file://${window.location.pathname.replace("/index.html",'')}/${book}'">${book}</a>`;
+	li.innerHTML = `<a href="${book}">${book}</a>`;
 	document.getElementById('programm-content-list').append(li);
 }
 for(let book of literature["Теория"]){
