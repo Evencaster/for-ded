f2607c90edb3eb12f1eff247c116631e369c68cf
mzhirko <mzhirko@gmail.com>
2020-04-10 13:24:19 +0300

    Add literature 'n some features

--- add_literature.js
+++ add_literature.js
@@ -1,21 +1,28 @@
 console.log(window.location.pathname.replace("/index.html",''));
 for(let book of literature["Программа"]) {
 	var li = document.createElement('li');
-	li.innerHTML = `<a href="${book}">${book}</a>`;
+	li.innerHTML = `<a href="ЭП/Программа дисциплины/${book}.pdf">${book}</a>`;
 	document.getElementById('programm-content-list').append(li);
 }
 for(let book of literature["Теория"]){
 	var li = document.createElement('li');
-	li.innerHTML = book;
+	li.innerHTML = `<a href="ЭП/Теория по курсу/${book}.pdf">${book}</a>`;
 	document.getElementById('theory-content-list').append(li);
 }
 for(let book of literature["Практика"]){
 	var li = document.createElement('li');
-	li.innerHTML = book;
+	li.innerHTML = `<a href="ЭП/Практика/${book}.pdf">${book}</a>`;
+	li.innerHTML = `<a href="ЭП/Практика/Цепи смещения МОП-транзисторов/${book}.pdf">${book}</a>`;
+	li.innerHTML = `<a href="ЭП/Практика/Усилительный каскад на МОП-транзисторе, включенном по схеме с общим исходом/${book}.pdf">${book}</a>`;
 	document.getElementById('practic-content-list').append(li);
+}
+for(let book of literature["Методическая литература"]){
+	var li = document.createElement('li');
+	li.innerHTML = `<a href="${book}">${book}</a>`;
+	document.getElementById('methodical-content-list').append(li);
 }
 for(let book of literature["Контроль знаний"]){
 	var li = document.createElement('li');
-	li.innerHTML = book;
+	li.innerHTML = `<a href="ЭП/Контроль знаний/${book}.pdf">${book}</a>`;
 	document.getElementById('control-content-list').append(li);
 }
